a0511828a801e412fefe9a1b94f5b2ac8ee1a39d
Tanvir0146 <tanvirhasan0146@gmail.com>
2021-05-23 20:30:17 +0600

    first commit

---
+++ Ex1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> My web page </title>
+</head>
+<body>
+<h1> my paragrapgh</h1>
+
+<p> my paragrapgh </p>
+
+<a href="https://www.aiub.edu/">AIUB </a>
+<img src="htdocs/MyLab1/download.jpg" width="150" height="130">
+<br><br>
+
+<form>
+
+First Name:
+<input type="text" id="fname" name="fname" placeholder="Enter First your name">
+Lat Name:
+<input type="text" id="f" name=" lname" placeholder="Enter  last your name">
+Middle name:
+<input type="text" id="me" name="middle name">
+Email:
+<input type="text" id="e" name="Email">
+Password:
+<input type="text" id="w" name="Password">
+<input type="Submit" value="Submit">
+<input type="reset" value="Reset">
+</form>
+<br>
+<h3> Interest Sectors </h3>
+<input type="radio" id="Sports" name="Interest" value="M">
+Sports
+<input type="radio" id="Music" name="Interest" value="l">
+Music
+<input type="radio" id="Singing" name="Interest" value="k">
+Singing
+<br>
+<h3> My personal Vehicle </h3>
+<input type="checkbox" id="Vehicle1" name="Vehicle1" value="Bike">
+<label for="Vehicle1"> I have a Bike </label><br>
+<input type="checkbox" id="Vehicle2" name="Vehicle2" value="Car">
+<label for="Vehicle2"> I have a Car </label><br>
+<input type="checkbox" id="Vehicle3" name="Vehicle3" value="Bus">
+<label for="Vehicle3"> I have a Bus </label><br>
+
+</body>
+
+</html>
